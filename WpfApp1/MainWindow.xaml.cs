2d3361b59b6d33971d69730c492a908245f7a0fe
Garrett <Garrettdeblois13@yahoo.com>
2018-10-07 22:17:33 -0500

    Experimenting with WPF. Created a calculator where all the operation buttons substitute an equals button. You use the operation button (+, -, *, /) after the number is entered, not before like on a traditional calculator.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace WpfApp1
     {
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();           // 
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
